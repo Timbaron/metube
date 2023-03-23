1806d1fe719f77214d994a3d592a8dd9c33657c0
timbaron <timothy.akiode@student.aul.edu.ng>
2023-03-23 12:49:16 +0100

    Initialize sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {Box, Stack, Typography} from '@mui/material'
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{flexDirection: {sx: 'column', md: 'row'}}}>
+      <Box sx={{height: {sx: 'auto', md: '92vh'},BorderRight: '1px solid #3d3d3d', px: {sx: 0, md:2}}}>
+        SideBar
+
+        <Typography className="compyright" varient="body2" sx={{mt:1.5, color: '#fff'}}>
+          Copyright 2023 Akiode Timothy
+        </Typography>
+      </Box>
+    </Stack>
   )
 }
 
